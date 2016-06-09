6e877b04e361243c3c22f60588b3285f7ed15356
turtlebot <mrsdteamg2015@gmail.com>
2016-06-08 20:04:43 -0400

    launch file, yaml file and costmap_2D

--- src/obstacle_detection/src/costmap_2D.cpp
+++ src/obstacle_detection/src/costmap_2D.cpp
@@ -2,10 +2,13 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <Eigen/Dense>
+#include <nav_msgs/OccupancyGrid.h>
 
 float alpha=0;
 float beta=0;
 float delta=0;
+nav_msgs::OccupancyGrid GridMap;
 class costmap_2D
 {
 private: 
@@ -13,20 +16,18 @@ private:
 		ros::NodeHandle nh;
 		float plane_coeffs[];
 		float resolution;
-		ros::Subscriber plane_coeff_sub;
 		ros::Subscriber cam_depth_pts_sub;
 	
 public:
 	costmap_2D();
 	~costmap_2D();
-	void plane_coeff_cb(const std_msgs::Float32MultiArray::ConstPtr& plane_coeffs);
+	void plane_coeff_cb();
 	void camera_cb(const sensor_msgs::PointCloud2::ConstPtr& msg);
 	
 };
 costmap_2D::costmap_2D()
 {
   cam_depth_pts_sub = nh.subscribe("/camera/depth_registered/points", 1000, &costmap_2D::camera_cb, this);
-  plane_coeff_sub = nh.subscribe("ground_plane",10,&costmap_2D::plane_coeff_cb, this);
 
 }
 void costmap_2D::camera_cb(const sensor_msgs::PointCloud2::ConstPtr& msg)
@@ -39,17 +40,14 @@ void costmap_2D::camera_cb(const sensor_msgs::PointCloud2::ConstPtr& msg)
 }
 
 
-void costmap_2D::plane_coeff_cb(const std_msgs::Float32MultiArray::ConstPtr& msg2)
+void costmap_2D::plane_coeff_cb()
 {
- std::vector<float>::const_iterator it = msg2->data.begin();
- alpha = *it;
- it++;
- beta = *it;
- it++;
- delta = *it;
+ ros::Param("alpha", alpha);
+ ros::Param("beta",beta);
+ ros::Param("gamma",gamma);
 }
 
-
+float calculate_height()
 
 int main (int argc, char **argv)
 {
