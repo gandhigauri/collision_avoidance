2a22a110865c678509c03690eba59aaad72c5b7a
gandhigauri <gandhigauri94@gmail.com>
2016-06-21 17:11:49 -0400

    removed the catkin workspace components

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gauri/multirobotcolvoid/collision_avoidance/src
-# Build directory: /home/gauri/multirobotcolvoid/collision_avoidance/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(obstacle_detection)
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/gauri/multirobotcolvoid/collision_avoidance/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/gauri/multirobotcolvoid/collision_avoidance/devel;/opt/ros/indigo".split(';') if "/home/gauri/multirobotcolvoid/collision_avoidance/devel;/opt/ros/indigo" != "" else []
