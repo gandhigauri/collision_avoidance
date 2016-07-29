ce0d695be3f1b867037e894aecc513bd6ab8c869
gandhigauri <gandhigauri94@gmail.com>
2016-07-29 17:15:46 -0400

    corrected the listen transform fn, added odom publishing

--- collvoid/collvoid_simple_global_planner/src/tf_extractor.cpp
+++ collvoid/collvoid_simple_global_planner/src/tf_extractor.cpp
@@ -1,9 +1,10 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/TransformStamped.h>
 
-ros::Publisher tf_pub;
+ros::Publisher tf_bp_pub, tf_ob_pub;
 ros::Subscriber tf_ob_sub, tf_mo_sub;
 
  void listen_transform( std::string parent,std::string child)
@@ -13,16 +14,17 @@ ros::Subscriber tf_ob_sub, tf_mo_sub;
   tf::StampedTransform transform;
   try
   {
-    listener.waitForTransform(child, parent, ros::Time(0), ros::Duration(10.0));
-    listener.lookupTransform(child, parent, ros::Time(0), transform);
+    listener.waitForTransform(parent, child, ros::Time(0), ros::Duration(10.0));
+    listener.lookupTransform(parent, child, ros::Time(0), transform);
     geometry_msgs::TransformStamped transform_stamped;
     tf::transformStampedTFToMsg(transform,transform_stamped);
-    tf_pub.publish(transform_stamped);   
+    tf_bp_pub.publish(transform_stamped);   
   }
   catch (tf::TransformException &ex)
   {
     ROS_ERROR("%s", ex.what());
   }
+  ros::spin();
   }
 }
 void send_transform(std::string parent, std::string child, tf::Transform transform)
@@ -34,10 +36,15 @@ void send_transform(std::string parent, std::string child, tf::Transform transfo
 
 void tf_ob_callback(const geometry_msgs::TransformStamped::ConstPtr & msg)
 {
+  nav_msgs::Odometry odom;
   tf::StampedTransform transform;
-  tf::transformStampedMsgToTF(*msg, transform);
+  geometry_msgs::TransformStamped ts = *msg;
+  tf::transformStampedMsgToTF(ts, transform);
   send_transform("/odom","/base_footprint",transform);
-
+  odom.header = ts.header;
+  odom.child_frame_id = ts.child_frame_id;
+  tf::poseTFToMsg(transform, odom.pose.pose);
+  tf_ob_pub.publish(odom);
 }
 
 
@@ -55,10 +62,10 @@ int main (int argc, char** argv)
 	ros::NodeHandle nh;
 	std::string child= "/plate_3_link";
 	std::string parent = "/base_footprint";
-  tf_pub = nh.advertise<geometry_msgs::TransformStamped>("/transform_bf_p3",1);
+  tf_bp_pub = nh.advertise<geometry_msgs::TransformStamped>("/transform_bf_p3",1);
+  tf_ob_pub = nh.advertise<nav_msgs::Odometry>("/overhead_odom",1);
   tf_ob_sub = nh.subscribe("/tranform_odom_bf",10,&tf_ob_callback);
   tf_mo_sub = nh.subscribe("/tranform_map_odom",10,&tf_mo_callback);
   listen_transform(parent,child);
-  ros::spin();
 }
 
