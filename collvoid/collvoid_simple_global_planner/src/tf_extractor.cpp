471bf1ef17e06bfb22b8934621ee172214d87286
gandhigauri <gandhigauri94@gmail.com>
2016-08-05 17:12:50 -0400

    tf_extractor version for running with modified amcl and robot pose ekf launch files, changed params for velocity

--- collvoid/collvoid_simple_global_planner/src/tf_extractor.cpp
+++ collvoid/collvoid_simple_global_planner/src/tf_extractor.cpp
@@ -7,6 +7,7 @@
 ros::Publisher tf_bp_pub, nav_ob_pub, initpose_mo_pub;
 tf::Transform prevTransform;
 ros::Time prevTime;
+bool publish_init_pose = true;
 double LINEAR_THRESHOLD = 0.04, ANGULAR_THRESHOLD = 0.02;
 
 void listen_transform(std::string parent, std::string child)
@@ -50,6 +51,16 @@ void tf_ob_callback(const geometry_msgs::TransformStamped::ConstPtr &msg)
     odom.twist.twist.angular.z = diffYaw/dt;
   if (std::abs(del_d) > LINEAR_THRESHOLD)
     odom.twist.twist.linear.x = del_d/dt;
+
+  //setting covariances
+  for (int i = 0; i<36; i++)
+  {
+    if (i%7==0)
+    {
+      odom.pose.covariance[i] = 0.001;
+      odom.twist.covariance[i] = 0.001;
+    }
+  }
   
   nav_ob_pub.publish(odom);
   prevTransform = currentTransform;
@@ -59,14 +70,25 @@ void tf_ob_callback(const geometry_msgs::TransformStamped::ConstPtr &msg)
 
 void tf_mo_callback(const geometry_msgs::TransformStamped::ConstPtr &msg)
 {
+  if (publish_init_pose)
+  {
   geometry_msgs::PoseWithCovarianceStamped initpose;
   geometry_msgs::TransformStamped ts = *msg;
   tf::StampedTransform transform;
   tf::transformStampedMsgToTF(ts, transform);
   initpose.header.stamp = ros::Time::now();
   initpose.header.frame_id = ts.header.frame_id;
-  tf::poseTFToMsg(transform, initpose.pose.pose); 
+  tf::poseTFToMsg(transform, initpose.pose.pose);
+  for (int i = 0; i<36; i++)
+  {
+    if (i%7==0)
+    {
+      initpose.pose.covariance[i] = 0.0001;
+    }
+  } 
   initpose_mo_pub.publish(initpose);
+  publish_init_pose = false;
+  }
 }
 
 
@@ -80,10 +102,10 @@ int main (int argc, char** argv)
 
   //all publishers and subscribers
   tf_bp_pub = nh.advertise<geometry_msgs::TransformStamped>("/transform_bf_p3", 1);
-  nav_ob_pub = nh.advertise<nav_msgs::Odometry>("/overhead_odom", 1);
-  initpose_mo_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/overhead_initial_pose", 1);
-  tf_ob_sub = nh.subscribe<geometry_msgs::TransformStamped>("/transform_odom_bf", 1, tf_ob_callback);
-  tf_mo_sub = nh.subscribe<geometry_msgs::TransformStamped>("/transform_map_odom", 1, tf_mo_callback);
+  nav_ob_pub = nh.advertise<nav_msgs::Odometry>("/overhead_odom", 1000);
+  initpose_mo_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/overhead_initial_pose", 1000);
+  tf_ob_sub = nh.subscribe<geometry_msgs::TransformStamped>("/transform_odom_bf", 1000, tf_ob_callback);
+  tf_mo_sub = nh.subscribe<geometry_msgs::TransformStamped>("/transform_map_odom", 1000, tf_mo_callback);
   
   //publishing static transform between plate3 and base footprint from tf tree
   std::string child = "/plate_3_link";
