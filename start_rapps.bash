3f17a2132766c4099eba46781cea1608d2d858c3
gandhigauri <gandhigauri94@gmail.com>
2016-08-18 13:21:20 -0400

    added visualization topics to bash

--- start_rapps.bash
+++ start_rapps.bash
@@ -6,7 +6,7 @@ RAPP="collvoid_rapps/shared_pose"
 
 for turtlebot in ${TURTLEBOTS}
 do
-  remappings="{remap_from: /position_share_out, remap_to: /${turtlebot}/position_share_out}, {remap_from: /position_share_in, remap_to: /${turtlebot}/position_share_in}, {remap_from: /commands_robot, remap_to: /${turtlebot}/commands_robot}, {remap_from: /transform_bf_p3, remap_to: /${turtlebot}/transform_bf_p3}, {remap_from: /transform_odom_bf, remap_to: /${turtlebot}/transform_odom_bf}, {remap_from: /transform_map_odom, remap_to: /${turtlebot}/transform_map_odom}"
+  remappings="{remap_from: /position_share_out, remap_to: /${turtlebot}/position_share_out}, {remap_from: /position_share_in, remap_to: /${turtlebot}/position_share_in}, {remap_from: /commands_robot, remap_to: /${turtlebot}/commands_robot}, {remap_from: /transform_bf_p3, remap_to: /${turtlebot}/transform_bf_p3}, {remap_from: /transform_odom_bf, remap_to: /${turtlebot}/transform_odom_bf}, {remap_from: /transform_map_odom, remap_to: /${turtlebot}/transform_map_odom}, {remap_from: /me, remap_to: /${turtlebot}/me}, {remap_from: /neighbors, remap_to: /${turtlebot}/neighbors}, {remap_from: /move_base/CollvoidLocalPlanner/global_plan, remap_to: /${turtlebot}/move_base/CollvoidLocalPlanner/global_plan}"
   parameters=""
   rosservice call /${turtlebot}/start_rapp "${RAPP}" ["${remappings}"] ["${parameters}"] &
 done
@@ -14,5 +14,6 @@ done
 wait
 
 exit 0
+
 
 
